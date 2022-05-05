e65f5d67e599a965800d3d22310e3ef4e8fc421d
Abhishek kumar singh <82291395+abhigeu9193@users.noreply.github.com>
2022-05-05 19:30:34 +0530

    Add files via upload

---
+++ EmployeeWageUsing-oops/Emp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*namespace EmployeeWageComputation
+{
+    internal class Emp
+    {
+        using System;*/
+using EmployeeWageComputation;
+class Program
+    {
+        public static void Main(String[] args)
+        {
+            EmployeeWage company = new EmployeeWage();
+            company.ComputeEmpWage();
+        }
+ }
---
+++ EmployeeWageUsing-oops/EmployeeWageBuilderObject.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//namespace EmployeeWageComputation
+//{
+   // internal class EmployeeWageBuilderObject
+   // {
+//using System;
+/*using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;*/
+
+namespace EmployeeWageComputation
+{
+        public class EmpWageBuilderObject
+        {
+            public string Company_Name { get; set; }
+            public int Emp_Wage_Pr_Hr { get; set; }
+            public int Emp_Max_Working_Hr { get; set; }
+            public int Emp_Working_Day_Pr_Month { get; set; }
+            public decimal Total_Emp_Wage { get; set; }
+            public decimal DailyEmployeeSalary { get; set; }
+
+        }
+}
